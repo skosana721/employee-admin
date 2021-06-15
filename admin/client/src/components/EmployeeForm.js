4be36553898b7ffcc3d84ad0c82616f329a28869
skosana721 <wonganiskosana@gmail.com>
2021-06-15 11:15:29 +0200

    added attribute for value and passed the formInfo values

--- admin/client/src/components/EmployeeForm.js
+++ admin/client/src/components/EmployeeForm.js
@@ -8,28 +8,38 @@ const EmployeeForm = () => {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="name"
           onChange={handleChange}
           placeholder="Enter employee name"
+          value={formInfo.name}
         />
         <input
           type="text"
+          name="surname"
           onChange={handleChange}
           placeholder="Enter employee surname"
+          value={formInfo.surname}
         />
         <input
           type="text"
+          name="idNumber"
           onChange={handleChange}
           placeholder="Enter employee ID number"
+          value={formInfo.idNumber}
         />
         <input
           type="number"
+          name="position"
           onChange={handleChange}
           placeholder="Enter employee job position"
+          value={formInfo.position}
         />
         <input
           type="number"
+          name="salary"
           onChange={handleChange}
           placeholder="Enter employee salary"
+          value={formInfo.salary}
         />
         <button type="submit">Submit</button>
       </form>
