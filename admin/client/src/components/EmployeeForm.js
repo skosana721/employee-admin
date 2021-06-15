764e40866374e99ddb2a4da4a87e65d8ab050892
skosana721 <wonganiskosana@gmail.com>
2021-06-15 10:49:51 +0200

    added placeholders to the inputs

--- admin/client/src/components/EmployeeForm.js
+++ admin/client/src/components/EmployeeForm.js
@@ -4,11 +4,11 @@ const EmployeeForm = () => {
   return (
     <div>
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="number" />
-        <input type="number" />
+        <input type="text" placeholder="Enter employee name" />
+        <input type="text" placeholder="Enter employee surname" />
+        <input type="text" placeholder="Enter employee ID number" />
+        <input type="number" placeholder="Enter employee job position" />
+        <input type="number" placeholder="Enter employee salary" />
         <button type="submit">Submit</button>
       </form>
     </div>
