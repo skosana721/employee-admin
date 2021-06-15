7b62d55936607eb8f87751b2e47a44f5f19d5299
skosana721 <wonganiskosana@gmail.com>
2021-06-15 11:03:13 +0200

    added state for formInfo and errors and returned them

--- admin/client/src/components/useForm.js
+++ admin/client/src/components/useForm.js
@@ -1,4 +1,14 @@
 import { useState, useEffect } from "react";
-const useForm = () => {};
+const useForm = () => {
+  const [formInfo, setformInfo] = useState({
+    name: "",
+    surname: "",
+    idNumber: "",
+    position: "",
+    salary: "",
+  });
+  const [errors, setErrors] = useState({});
+  return { formInfo, errors };
+};
 
 export default useForm;
