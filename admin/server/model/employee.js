20c6f4281f08bd505bd1601e5961cc6dba2d2c8d
skosana721 <wonganiskosana@gmail.com>
2021-06-15 14:48:57 +0200

    created an EmployeeSchema and Employee model

---
+++ admin/server/model/employee.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  idNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: String,
+    required: true,
+  },
+});
+
+const Employee = mongoose.model("Employee", EmployeeSchema);
+
+module.exports = Employee;
