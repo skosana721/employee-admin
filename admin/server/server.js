bd0057da13859ff2a1b3663fb1d82b8a7cda3def
skosana721 <wonganiskosana@gmail.com>
2021-06-15 14:58:02 +0200

    required the employeeRoutes and invoked it and passed the app variable as the argument

--- admin/server/server.js
+++ admin/server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const { employeeRoutes } = require("./routes/api/employee");
 
 require("dotenv/config");
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+employeeRoutes(app);
 app.listen(5050, () => console.log(`Server is running on port 5050...`));
