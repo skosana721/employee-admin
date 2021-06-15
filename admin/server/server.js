264ac46ba23d2e2ed988cd65cc2e71c5253428b8
skosana721 <wonganiskosana@gmail.com>
2021-06-15 14:40:54 +0200

    required mongoose and used it to connect the database

--- admin/server/server.js
+++ admin/server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 require("dotenv/config");
-
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.listen(5050, () => console.log(`Server is running on port 5050...`));
