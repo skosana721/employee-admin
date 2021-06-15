34f1e3ee638f13e6d9dad1fcdd5759f87f3e385e
skosana721 <wonganiskosana@gmail.com>
2021-06-15 10:45:54 +0200

    rendered the EmployeeForm component

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import EmployeeForm from "./components/EmployeeForm";
 
 function App() {
   return (
     <div className="App">
       <h1>React</h1>
+      <EmployeeForm />
     </div>
   );
 }
