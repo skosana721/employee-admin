2b60b52a3f313b9d5c9e6abe2a77e5cd785faf79
skosana721 <wonganiskosana@gmail.com>
2021-06-15 11:09:33 +0200

    import the useForm and deconstructed the handleChange, handleSubmit , formInfo and errors from useForm method

--- admin/client/src/components/EmployeeForm.js
+++ admin/client/src/components/EmployeeForm.js
@@ -1,6 +1,8 @@
 import React from "react";
+import useForm from "./useForm";
 
 const EmployeeForm = () => {
+  const { handleChange, formInfo, errors, handleSubmit } = useForm();
   return (
     <div>
       <form>
