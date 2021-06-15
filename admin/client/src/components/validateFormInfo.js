fb44339c6432ac6009c1bf833af773313a17da58
skosana721 <wonganiskosana@gmail.com>
2021-06-15 12:22:11 +0200

    added inline style to the p tag

--- admin/client/src/components/validateFormInfo.js
+++ admin/client/src/components/validateFormInfo.js
@@ -12,7 +12,7 @@ export const validFormInfo = (form) => {
   if (!form.salary) {
     errors.salary = `Salary is required`;
   } else if (form.salary < 3500) {
-    errors.salary = `Salary is below the minimum`;
+    errors.salary = `Salary is below the minimum, <p style='color:blue'>3500</p> `;
   }
 
   return errors;
