3e00a6175ce114f2faf69803672df568156846e0
skosana721 <wonganiskosana@gmail.com>
2021-06-17 10:34:00 +0200

    created a post request that creates a new employee and sends the and send the created employee as a response with a status 201

--- admin/server/routes/api/employee.js
+++ admin/server/routes/api/employee.js
@@ -1,6 +1,24 @@
+const Employee = require("../../model/employee");
+
 const employeeRoutes = (app) => {
   app.get("/employee", (req, res) => {
     res.send("It works");
+  });
+  app.post("/employee", async (req, res) => {
+    const { name, surname, idNumber, position, salary } = req.body;
+    try {
+      const newEmployee = await Employee({
+        name,
+        surname,
+        position,
+        idNumber,
+        position,
+        salary,
+      }).save();
+      res.status(201).send(newEmployee);
+    } catch (error) {
+      res.status(400).send(error);
+    }
   });
 };
 module.exports = { employeeRoutes };
