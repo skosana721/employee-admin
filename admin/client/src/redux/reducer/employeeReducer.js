f65b072d2e20ba5e4a3bd706c9244bcda153a923
skosana721 <wonganiskosana@gmail.com>
2021-06-17 11:45:30 +0200

    added case for DISPLAY_EMPLOYEES which spreads the state and adds a payload to the employees array

--- admin/client/src/redux/reducer/employeeReducer.js
+++ admin/client/src/redux/reducer/employeeReducer.js
@@ -1,8 +1,15 @@
+import { DISPLAY_EMPLOYEES } from "../actionTypes/employee";
+
 const initialState = {
   employees: [],
 };
 export const employeeReducer = (state = initialState, action) => {
   switch (action.type) {
+    case DISPLAY_EMPLOYEES:
+      return {
+        ...state,
+        employees: [...state.employees, action.payload],
+      };
     default:
       return state;
   }
