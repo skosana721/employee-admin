02d6abbd35be41b7dde6746098948f5ebb5cd0f9
skosana721 <wonganiskosana@gmail.com>
2021-06-17 11:19:38 +0200

    imported the employeeReducer and passed it as an argument in the createStore method

--- admin/client/src/redux/store/index.js
+++ admin/client/src/redux/store/index.js
@@ -1,6 +1,9 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-
+import { employeeReducer } from "../reducer/employeeReducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(
+  employeeReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
