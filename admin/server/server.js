7b18db16e3152ad1e26ee3b8a3e2e26dd14d5ca4
skosana721 <wonganiskosana@gmail.com>
2021-06-17 10:29:05 +0200

    aaded express bodyparser

--- admin/server/server.js
+++ admin/server/server.js
@@ -8,5 +8,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 employeeRoutes(app);
 app.listen(5050, () => console.log(`Server is running on port 5050...`));
