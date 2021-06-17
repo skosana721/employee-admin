8836801b416f1ca286ea0b381690a92441b18349
skosana721 <wonganiskosana@gmail.com>
2021-06-17 11:18:26 +0200

    added a employeeReducer file which initial returns the state as default case

---
+++ admin/client/src/redux/reducer/employeeReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  employees: [],
+};
+export const employeeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
