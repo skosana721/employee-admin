159c740e1675cb8f9047b4737a435060a675911a
skosana721 <wonganiskosana@gmail.com>
2021-06-17 11:42:05 +0200

    fixed spelling mistake

--- admin/client/src/redux/actions/employee.js
+++ admin/client/src/redux/actions/employee.js
@@ -1,9 +1,9 @@
-import axois from "axios";
+import axios from "axios";
 import { DISPLAY_EMPLOYEES } from "../actionTypes/employee";
 
 export const displayEmployees = () => {
   return (dispatch) => {
-    axois.get("http://localhost:5050/employee").then((res) => {
+    axios.get("http://localhost:5050/employee").then((res) => {
       dispatch({
         type: DISPLAY_EMPLOYEES,
         payload: res.data,
