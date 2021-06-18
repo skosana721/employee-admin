aaf8dc0f235304f20313d0a0f8ba280c8a0ea9be
skosana721 <wonganiskosana@gmail.com>
2021-06-18 12:21:49 +0200

    rendered the name , surname, position, idNumber, and salary

--- admin/client/src/components/Employee.js
+++ admin/client/src/components/Employee.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Employee = () => {
-  return <div></div>;
+const Employee = ({ name, surname, idNumber, position, salary }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{surname}</td>
+      <td>{idNumber}</td>
+      <td>{position}</td>
+      <td>{salary}</td>
+    </tr>
+  );
 };
 
 export default Employee;
