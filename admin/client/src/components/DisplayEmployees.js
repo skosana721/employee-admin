dcd0ae5d645e398c570cd3bbeb7e74fc0f6cae06
skosana721 <wonganiskosana@gmail.com>
2021-06-18 12:10:37 +0200

    selected the employees state from redux and mapped it and returned the Employee component and passed the employee as prop

--- admin/client/src/components/DisplayEmployees.js
+++ admin/client/src/components/DisplayEmployees.js
@@ -1,13 +1,40 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getEmployees } from "../redux/actions/employee";
+import Employee from "./Employee";
+import { Table } from "reactstrap";
 
 const DisplayEmployees = () => {
+  const employees = useSelector((state) => state.employees);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getEmployees());
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>ID Number</th>
+            <th>Position</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map((employee) => {
+            return (
+              <div>
+                <Employee key={employee._id} {...employee} />
+              </div>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default DisplayEmployees;
