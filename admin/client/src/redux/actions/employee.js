a85fff6e66b8b72cc607bcb2bc649131b5eae5cb
skosana721 <wonganiskosana@gmail.com>
2021-06-18 10:49:47 +0200

    created an action for addEmployee which takes form as its parameter

--- admin/client/src/redux/actions/employee.js
+++ admin/client/src/redux/actions/employee.js
@@ -1,11 +1,22 @@
 import axios from "axios";
-import { GET_EMPLOYEES } from "../actionTypes/employee";
+import { ADD_EMPLOYEE, GET_EMPLOYEES } from "../actionTypes/employee";
 
 export const getEmployees = () => {
   return (dispatch) => {
     axios.get("http://localhost:5050/employee").then((res) => {
       dispatch({
         type: GET_EMPLOYEES,
+        payload: res.data,
+      });
+    });
+  };
+};
+
+export const addEmployee = (form) => {
+  return (dispatch) => {
+    axios.post("http://localhost:5050/employee", form).then((res) => {
+      dispatch({
+        type: ADD_EMPLOYEE,
         payload: res.data,
       });
     });
