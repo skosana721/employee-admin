3da27bfcf3551a68d7bc66b7caeab428d58c3aa6
skosana721 <wonganiskosana@gmail.com>
2021-06-18 10:23:54 +0200

    imported the getEmployees action from redux and called the useEffect to dispatch the getEmployees

--- admin/client/src/components/DisplayEmployees.js
+++ admin/client/src/components/DisplayEmployees.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { getEmployees } from "../redux/actions/employee";
 
 const DisplayEmployees = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getEmployees());
+  }, []);
   return <div></div>;
 };
 
