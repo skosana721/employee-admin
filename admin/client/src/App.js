7c5a5c989204133067f772af381497ecf98a9af8
skosana721 <wonganiskosana@gmail.com>
2021-06-18 10:20:22 +0200

    rendered the DisplayEmployees component

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import EmployeeForm from "./components/EmployeeForm";
+import DisplayEmployees from "./components/DisplayEmployees";
 
 function App() {
   return (
     <div className="App">
       <h1>React</h1>
       <EmployeeForm />
+      <DisplayEmployees />
     </div>
   );
 }
