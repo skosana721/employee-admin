ea9270e8ec6440133b2cb2732823ebec0e299d1a
skosana721 <wonganiskosana@gmail.com>
2021-06-22 11:18:41 +0200

    added variable idTest which use regex to check if the id is a valid id and added an if statement to check if the inputed id is a vaild one and added a error message if not true

--- admin/client/src/components/validateFormInfo.js
+++ admin/client/src/components/validateFormInfo.js
@@ -1,5 +1,8 @@
 export const validFormInfo = (form) => {
   let errors = {};
+  const idTest =
+    /^(((\d{2}((0[13578]|1[02])(0[1-9]|[12]\d|3[01])|(0[13456789]|1[012])(0[1-9]|[12]\d|30)|02(0[1-9]|1\d|2[0-8])))|([02468][048]|[13579][26])0229))(( |-)(\d{4})( |-)(\d{3})|(\d{7}))/;
+
   if (!form.name.trim()) {
     errors.name = `Name is required`;
   }
@@ -11,6 +14,8 @@ export const validFormInfo = (form) => {
   }
   if (!form.idNumber) {
     errors.idNumber = `ID number is required`;
+  } else if (!idTest.test(form.idNumber)) {
+    errors.idNumber = `Invalid SA ID number`;
   }
   if (!form.salary) {
     errors.salary = `Salary is required`;
