0c97c0e9ac5feca5ee9acc72b29331532b7f4842
skosana721 <wonganiskosana@gmail.com>
2021-06-23 09:25:34 +0200

    rendred the NavBar component

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import EmployeeForm from "./components/EmployeeForm";
 import DisplayEmployees from "./components/DisplayEmployees";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Switch>
           <Route path="/" exact component={EmployeeForm} />
           <Route path="/displayEmployees" component={DisplayEmployees} />
