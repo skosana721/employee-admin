e03bfb84044aa6f99fd6f245ed0996dbeb5df5c3
skosana721 <wonganiskosana@gmail.com>
2021-06-28 11:36:10 +0200

    imported the Home component and added a route to be the home page

--- admin/client/src/App.js
+++ admin/client/src/App.js
@@ -3,13 +3,15 @@ import EmployeeForm from "./components/EmployeeForm";
 import DisplayEmployees from "./components/DisplayEmployees";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Switch>
-          <Route path="/" exact component={EmployeeForm} />
+          <Route path="/" exact component={Home} />
+          <Route path="/EmployeeForm" exact component={EmployeeForm} />
           <Route path="/displayEmployees" component={DisplayEmployees} />
         </Switch>
       </Router>
